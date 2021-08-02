b23ee0f32b5411fb63e86e21cc18c467159a7432
PawelKonie99 <pawel.konieczny21@gmail.com>
2021-08-02 20:43:11 +0200

    Implement getting all data from backend

--- frontend/src/services/getAllData.tsx
+++ frontend/src/services/getAllData.tsx
@@ -1,9 +1,14 @@
 import axios from "axios";
+import { Category } from "../interfaces/category";
 const allDataUri = "/data";
 
-export const getAll = async () => {
-  const request = await axios.get(allDataUri);
-  return request.data;
+export interface Root extends Category {
+  data: Category[];
+}
+
+export const getAll = async (): Promise<Root> => {
+  const request: Root = await axios.get(allDataUri);
+  return request;
 };
 
 export default { getAll };
